3325fd9d121115c033f5dce7eb4627384ddc44e2
Lajancia <50996139+Lajancia@users.noreply.github.com>
2023-02-19 17:11:16 +0900

    2023/02/19

---
+++ src/layouts/Home.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function BasicGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid xs={12} >
+        <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
+        </Grid>
+     
+      </Grid>
+    </Box>
+  );
+}
